8f7275f8bd4321d59ec018ad06245c5ffcd32de9
Jake Pogue <bb2jake@gmail.com>
2017-09-18 20:02:13 -0600

    Style work. Needs more.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,61 @@
 function scrollNav() {
-    $('.nav a').click(function () {
-        //Toggle Class
-        $(".active").removeClass("active");
-        $(this).closest('li').addClass("active");
-        var theClass = $(this).attr("class");
-        $('.' + theClass).parent('li').addClass('active');
-        //Animate
-        $('html, body').stop().animate({
-            scrollTop: $($(this).attr('href')).offset().top - 50
-        }, 400);
-        return false;
-    });
-    $('.scrollTop a').scrollTop();
+	$('.nav a').click(function () {
+		//Toggle Class
+		$(".active").removeClass("active");
+		$(this).closest('li').addClass("active");
+		var theClass = $(this).attr("class");
+		$('.' + theClass).parent('li').addClass('active');
+		//Animate
+		$('html, body').stop().animate({
+			scrollTop: $($(this).attr('href')).offset().top - 50
+		}, 400);
+		return false;
+	});
+	$('.scrollTop a').scrollTop();
 }
 scrollNav();
-Chart.defaults.global.legend.display = false;
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        backgroundColor: 'rgba(0, 0, 0, 1)',
-        borderColor: 'red',
-        datasets: [{
-            backgroundColor: [
-                'red',
-                'white'
-            ],
-            data: [70, 30]
-        }],
-        borderWidth: [100, 100]
-    },
-    options: {
-        events: [],
-        cutoutPercentage: 70,
-        animation: {
-            animateRotate: false,
-            animateScale: false
-        }
-    }
-})
+
+// Progress bars
+$('.skill-bar').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+	if (visible) {
+		$.each($('div.progress-bar'), function () {
+			$(this).css('width', $(this).attr('aria-valuenow') + '%');
+		});
+		$(this).unbind('inview');
+	}
+});
+
+// More skills pie charts
+var once = false;
+$('.more-skill').bind('inview', function (event, visible, visiblePartX, visiblePartY) {
+	if (visible) {
+		$('.chart').easyPieChart({
+			easing: 'easeOut',
+			delay: 3000,
+			barColor: '#337ab7',
+			trackColor: 'rgba(255,255,255,0.2)',
+			scaleColor: false,
+			lineWidth: 8,
+			size: 140,
+			animate: 2000,
+		});
+		if (!once) {
+			addMoreSkillTitles();
+			once = true;
+		}
+		$(this).unbind('inview');
+	}
+});
+
+function addMoreSkillTitles() {
+	let skills = ['HTML5', 'CSS3', 'Bootstrap', 'jQuery', 'Source Control: Git/Perforce', 'MongoDB']
+	let elems = document.getElementsByClassName('chart');
+	for (let i = 0; i < elems.length && i < skills.length; i++) {
+		let elem = elems[i];
+		let div = document.createElement('div');
+		div.classList.add('chart-text');
+		div.classList.add('text-center');
+		div.innerHTML = `<span>${skills[i]}</span>`
+		elem.appendChild(div);
+	}
+}
